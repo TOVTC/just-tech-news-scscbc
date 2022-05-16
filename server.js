cc040e79a3f6c3a8124099cad7aaa5e11a194675
TOVTC <veronica.tc.to@outlook.com>
2022-05-16 05:56:26 -0400

    minor updates

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(express.urlencoded({extended: true}));
 // turn on routes
 app.use(routes);
 
-// turn on connection to db and server
+// turn on connection to db and server (force: true forces the the database to recreate tables if any of the associates in models/index.js are changed)
+// should usually be set to false so it doesn't drop tables every time - force: true is similar to DROP IF EXISTS
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => console.log("Now listening"));
 });
