3c9dbfa3ad685c27da518ccf66a5994674e803cd
TOVTC <veronica.tc.to@outlook.com>
2022-05-22 01:21:41 -0400

    added dashboard, add, edit, and delete posts functions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const {User, Post, Vote, Comment} = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -118,7 +119,7 @@ router.post('/logout', (req,res) =>{
     }
 });
 // PUT /api/users/1
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     // expects {username: '', email: '', password: ''}
     // if req.boyd has exact key/value pairs to math the model, you can just use 'req.body' instead
     User.update(req.body, {
@@ -141,7 +142,7 @@ router.put('/:id', (req, res) => {
 });
 
 // DELETE /api/users/1
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     User.destroy({
         where: {
             id: req.params.id
